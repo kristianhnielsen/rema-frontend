72675267669180df14f44f1c78e97a117e535a9d
Kristian <kristianhnielsen@protonmail.com>
2024-12-11 16:32:37 +0100

    allow remote img urls

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*rema1000.dk",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
