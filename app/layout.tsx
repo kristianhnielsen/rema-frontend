4284b6cbd341fbd4e4c1d398708f62e78154b286
Kristian <kristianhnielsen@protonmail.com>
2024-12-14 13:35:06 +0100

    change heading text

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarProvider>
           <AppSidebar />
           <main>
-            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-              <h1 className="text-2xl font-semibold text-gray-900 mb-6">
-                Product List
+            <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-semibold text-gray-900 mb-6 border-b-4">
+                Products
               </h1>
               {children}
             </div>
