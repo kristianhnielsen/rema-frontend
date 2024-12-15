b3aa1f8bb0cda367324fdd18e48a9b4b1caca65a
Kristian <kristianhnielsen@protonmail.com>
2024-12-15 22:09:40 +0100

    add header with logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/Sidebar";
+import RemaLogo from "@/components/RemaLogo";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,11 +9,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body>
         <SidebarProvider>
           <AppSidebar />
-          <main>
-            <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
-              {children}
-            </div>
-          </main>
+          <div className="max-w-7xl py-6 sm:px-6 lg:px-8">
+            <header className="flex items-center gap-4">
+              <RemaLogo />
+              <h1 className="text-5xl font-semibold">
+                Rema 1000 Price Tracker
+              </h1>
+            </header>
+            <main>
+              <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
+                {children}
+              </div>
+            </main>
+          </div>
         </SidebarProvider>
       </body>
     </html>
