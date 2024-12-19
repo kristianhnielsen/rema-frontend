9faf81cc845e06a2e50a0031fe817bae1bd53bac
Kristian <kristianhnielsen@protonmail.com>
2024-12-19 13:09:16 +0100

    fix header move on scroll before stuck on top

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body>
         <SidebarProvider className="block md:flex">
           <AppSidebar />
-          <div className="max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <header className="flex justify-between sticky top-0 bg-white py-4 items-center gap-4 border-b-4 border-rema-primary">
+          <div className="max-w-7xl px-4 pb-6 sm:px-6 lg:px-8">
+            <header className="flex justify-between sticky top-0 bg-white py-4 z-50 items-center gap-4 border-b-4 border-rema-primary">
               <div className="flex gap-4 w-full items-center">
                 <RemaLogo />
                 <h1 className="text-lg md:text-3xl font-semibold leading-tight">
