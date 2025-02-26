816a51da7c823b78cbb810808ba24d9cbc22ddd7
Kristian <kristianhnielsen@protonmail.com>
2025-02-26 21:45:24 +0100

    move current home page to main product page

---
+++ app/produkt/page.tsx
@@ -0,0 +1,33 @@
+import PageTitle from "@/components/PageTitle";
+import Pagination from "@/components/Pagination";
+import ProductCard from "@/components/ProductCard";
+import { fetchProducts, fetchProductsCount } from "@/lib/api";
+import { Product } from "@/types/api";
+
+const ITEMS_PER_PAGE = 20;
+
+export default async function AllProducts({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+  const page = typeof params.page === "string" ? parseInt(params.page, 10) : 1;
+  const offset = (page - 1) * ITEMS_PER_PAGE;
+
+  const products: Product[] = await fetchProducts(offset, ITEMS_PER_PAGE);
+  const totalProducts = await fetchProductsCount();
+  const totalPages = Math.ceil(totalProducts / ITEMS_PER_PAGE);
+
+  return (
+    <>
+      <PageTitle>Alle produkter</PageTitle>
+      <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <Pagination currentPage={page} totalPages={totalPages} baseUrl="/" />
+    </>
+  );
+}
